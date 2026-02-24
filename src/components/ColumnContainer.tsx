d230d72adad21d9d3082a6b858beec3d437f6f5d
Shubham Sinha <shubhamsinha@Shubhams-MacBook-Air.local>
2026-02-24 11:22:08 +0530

    ### Summary of Changes
    * **Improved Drag-and-Drop Functionality**: Enhanced the ColumnContainer component to support drag-and-drop functionality for reordering tickets between columns.
    * **Enhanced Menu Item Styling**: Added hover effect to menu items in the MenuItem component.

--- src/components/ColumnContainer.tsx
+++ src/components/ColumnContainer.tsx
@@ -1,28 +1,57 @@
 import React from "react";
 import Column from "./Column";
-import TicketCard from "./TicketCard";
+import { IBoard, ITicket, IUpdateTicketStatus } from "../interface/interfaces";
+import { moveTicket } from "../redux/boardSlice/boardsSlice";
+import { setActiveBoardId } from "../redux/boardSlice/activeBoardSlice";
+import { DragDropContext } from "react-beautiful-dnd";
+import { useDispatch } from "react-redux";
 
-function ColumnContainer() {
+function ColumnContainer({ board }) {
+  const dispatch = useDispatch();
+  //Segregate tickets
+  const currentBoard: IBoard = { ...board };
+  const columnWiseGroupedTickets = currentBoard.columns.map((column) => {
+    return {
+      columnId: column.id,
+      columnName: column.name,
+      tickets: currentBoard.tickets.filter(
+        (ticket) => ticket.status === column.id,
+      ),
+    };
+  });
+
+  const handleDragEnd = (result) => {
+    const { destination, source, draggableId } = result;
+
+    const ticket: ITicket = currentBoard.tickets.find(
+      (ticket) => ticket.id === draggableId,
+    ) as ITicket;
+
+    if (destination !== source) {
+      const moveEvent: IUpdateTicketStatus = {
+        boardId: currentBoard.id,
+        ticketId: ticket.id,
+        currentTicketStatus: source.droppableId,
+        nextTicketStatus: destination.droppableId,
+      };
+
+      dispatch(moveTicket(moveEvent));
+    }
+  };
+
   return (
     <div className="mt-6 grid h-full grid-cols-5">
-      <Column label={"Todo"}>
-        <TicketCard />
-        <TicketCard />
-        <TicketCard />
-      </Column>
-      <Column label={"In Dev"}>
-        <TicketCard />
-      </Column>
-      <Column label={"Code Review"}>
-        <TicketCard />
-      </Column>
-      <Column label={"In QA"}>
-        <TicketCard />
-        <TicketCard />
-      </Column>
-      <Column label={"Release Done"}>
-        <TicketCard />
-      </Column>
+      <DragDropContext onDragEnd={handleDragEnd}>
+        {columnWiseGroupedTickets &&
+          columnWiseGroupedTickets.map((columnData) => (
+            <Column
+              key={columnData.columnId}
+              columnId={columnData.columnId}
+              label={columnData.columnName}
+              tickets={columnData.tickets}
+            />
+          ))}
+      </DragDropContext>
     </div>
   );
 }
