05f8f6d513520d8e1ad4d54ca023eafe121f343e
Shubham Sinha <shubhamsinha@Shubhams-MacBook-Air.local>
2026-02-24 14:51:47 +0530

    ### Summary of Changes
    * Added drag-and-drop functionality to ticket cards.
    * Introduced a new interface for state management and added several new interfaces for data models.
    * Enhanced the Create Board page with improved user experience and functionality, including:
    	+ Selecting a manager from a list of available users.
    	+ Defining columns for the board.
    	+ Adding and deleting columns dynamically.
    	+ Creating a new board with the selected manager and columns.

---
+++ src/interface/interfaces.ts
@@ -0,0 +1,90 @@
+export interface IState {
+    activeBoardId?: any;
+    boards?: IBoard[];
+    masterData?: IMasterData;
+}
+
+export interface IMasterData {
+    users?: IUser[];
+    status?: string[];
+    storyPoints?: StoryPoint[];
+    priority?: string[];
+    labels?: string[];
+}
+
+export interface StoryPoint {
+    id: string;
+    name: number;
+}
+
+export interface IBoard {
+    id: string;
+    name: string;
+    slug: string;
+    manager: IUser;
+    columns: IColumn[];
+    tickets: ITicket[];
+}
+
+export interface IColumn {
+    id: string;
+    boardId: string;
+    name: string;
+    slug: string;
+    tickets: ITicket[];
+}
+
+export interface ITicket {
+    id: string;
+    boardId: string;
+    columnId: string;
+    title: string;
+    description: string;
+    assignee: IUser;
+    status: string;
+    storyPoint: number;
+    labels: string[];
+    priority: string;
+    dueDate: string;
+    createdAt: Date;
+    updatedAt: Date;
+}
+
+export interface IUser {
+    id: string;
+    name: string;
+    email: string;
+    role: string;
+    avatarUrl?: string;
+}
+
+export interface ICreateBoard {
+    name: string;
+    slug: string;
+    manager: IUser;
+    columns: string[];
+}
+
+export interface ICreateTicket {
+    title: string;
+    description: string;
+    boardId: string;
+    assignee: IUser;
+    status: string;
+    storyPoints?: number;
+    labels?: string[];
+    priority?: string;
+    dueDate?: string;
+}
+
+export interface KeyValue {
+    id: string;
+    name: string;
+}
+
+export interface IUpdateTicketStatus {
+    boardId: string;
+    ticketId: string;
+    currentTicketStatus: string;
+    nextTicketStatus: string;
+}
